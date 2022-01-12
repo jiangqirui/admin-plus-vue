0be03749317bfc9e6a0dce2bc25860c04fb1c8f3
qirui <1562653352@qq.com>
2022-01-12 10:02:43 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import routers from "./routers/routers";
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+    mode: 'history',
+    routes: routers
+})
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount("#app")
